4caf71252838bf8165ea3fe9e73881d8f3af00d0
Calvin Liang <cliangdev@gmail.com>
2026-04-15 21:34:29 -0700

    fix: add missing EventType import to IssueServiceTest

--- conductor-backend/src/test/java/com/conductor/service/IssueServiceTest.java
+++ conductor-backend/src/test/java/com/conductor/service/IssueServiceTest.java
@@ -27,6 +27,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.conductor.notification.EventType;
 import com.conductor.notification.NotificationEvent;
 
 import java.time.OffsetDateTime;
